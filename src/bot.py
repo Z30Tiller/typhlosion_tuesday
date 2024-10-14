065e2396867df43ada93b996ec9f6b8f5aaf93d5
Z30Tiller <143367856+Z30Tiller@users.noreply.github.com>
2024-10-14 15:02:52 +1300

    Updated code and zip

--- src/bot.py
+++ src/bot.py
@@ -35,12 +35,6 @@ wednesdayGifs = [
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user.name}')
-
-    # # Schedule the job to run every Tuesday at 12:00 PM
-    # scheduler = AsyncIOScheduler()
-    # scheduler.add_job(post_tue_gifs, 'cron', day_of_week='tue', hour=10, minute=0)
-    # scheduler.add_job(post_wed_gifs, 'cron', day_of_week='wed', hour=10, minute=0)
-    # scheduler.start()
     
     # Get the current date
     today = datetime.datetime.now()
@@ -61,23 +55,27 @@ async def on_ready():
                 await channel.send(gif)
         elif day_of_week == 2:
             for gif in wednesdayGifs:
-                await channel.send(gif) 
-
-@bot.event
-async def on_guild_join(guild):
-    # This event is triggered when the bot joins a new server
-    print(f"Bot added to server: {guild.name} (ID: {guild.id})")
-
-    # You can choose a default channel to send a message to or configure further
-    # Generally, try to find the 'general' text channel or the first channel
-    for channel in guild.text_channels:
-        if "general" in channel.name:
-            await channel.send("Hello! Thanks for adding me to your server!")
-            break
+                await channel.send(gif)
         else:
-            # Fallback to the first available text channel
-            first_channel = guild.text_channels[0]
-            await first_channel.send("Hello! Thanks for adding me to your server!")
+            print(f'Day of the week does not match')
+    else:
+        print(f'Channel not loaded correctly')
+
+# @bot.event
+# async def on_guild_join(guild):
+#     # This event is triggered when the bot joins a new server
+#     print(f"Bot added to server: {guild.name} (ID: {guild.id})")
+
+#     # You can choose a default channel to send a message to or configure further
+#     # Generally, try to find the 'general' text channel or the first channel
+#     for channel in guild.text_channels:
+#         if "general" in channel.name:
+#             await channel.send("Hello! Thanks for adding me to your server!")
+#             break
+#         else:
+#             # Fallback to the first available text channel
+#             first_channel = guild.text_channels[0]
+#             await first_channel.send("Hello! Thanks for adding me to your server!")
 
 # @bot.command
 # async def set_channel(ctx, channel: discord.TextChannel):
@@ -86,40 +84,9 @@ async def on_guild_join(guild):
 #         f.write(str(channel.id))
 #     await ctx.send(f"Channel set to: {channel.name}")
 
-# async def post_tue_gifs():
-#     # Read the saved channel ID
-#     with open("channel_config.txt", "r") as f:
-#         channel_id = int(f.read())
-
-#     channel = bot.get_channel(channel_id)
-#     if channel:
-#         for gif in tuesdayGifs:
-#             await channel.send(gif)
-
-# async def post_wed_gifs():
-#     # Read the saved channel ID
-#     with open("channel_config.txt", "r") as f:
-#         channel_id = int(f.read())
-
-#     channel = bot.get_channel(channel_id)
-#     if channel:
-#         for gif in wednesdayGifs:
-#             await channel.send(gif)
-
-# @bot.command
-# async def post_test(ctx):
-#     # Read the saved channel ID
-#     with open("channel_config.txt", "r") as f:
-#         channel_id = int(f.read())
-
-#     channel = bot.get_channel(channel_id)
-#     if channel:
-#         channel.send("This is a test!")
-
-
 # Lambda handler function
 def lambda_handler(event, context):
     token = os.environ['DISCORD_TOKEN']
     bot.run(TOKEN)
 
-bot.run(os.environ['DISCORD_TOKEN'])
+# bot.run(os.environ['DISCORD_TOKEN'])
